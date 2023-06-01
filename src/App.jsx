71fd7851da5bfc4a6ad48d8bcc7571f489905b56
Roman <romansmerdyakov@gmail.com>
2023-06-01 10:24:24 +0600

    creaate Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { Box, Stack } from '@mui/material';
 import Feed from './components/Feed';
 import Rightbar from './components/Rightbar';
 import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <>
-      <Sidebar />
-      <Feed />
-      <Rightbar />
-    </>
+    <Box>
+      <Navbar />
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,118 @@
-import React from 'react';
+// import styled from '@emotion/styled';
+import { styled } from '@mui/material/styles';
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  InputBase,
+  Badge,
+  Avatar,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+
+import { Pets, Mail, Notifications } from '@mui/icons-material';
+import { useState } from 'react';
+
+const StyledToolbar = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+
+const Search = styled('div')(({ theme }) => ({
+  backgroundColor: 'white',
+  padding: '0 10px',
+  borderRadius: theme.shape.borderRadius,
+  width: '40%',
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: 'none',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '20px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex',
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '10px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'none',
+  },
+}));
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography
+          variant="h6"
+          sx={{
+            display: {
+              xs: 'none',
+              sm: 'block',
+            },
+          }}
+        >
+          RemaDev
+        </Typography>
+        <Pets
+          sx={{
+            display: {
+              xs: 'block',
+              sm: 'none',
+            },
+          }}
+        />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={2} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={3} color="success">
+            <Notifications />
+          </Badge>
+          <Avatar
+            onClick={() => setOpen(true)}
+            src="https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />
+        </Icons>
+        <UserBox onClick={() => setOpen(true)}>
+          <Avatar src="https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+          <Typography variant="span">RemaDev</Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id="basic-menu"
+        open={isOpen}
+        onClose={() => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
 };
 
 export default Navbar;
